bdd3ebb1a2008515eae80f3739d3a04312bdbaf0
Roman Schigolev <r.shchigolev2018@yandex.ru>
2023-01-13 21:24:49 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Toaster } from 'react-hot-toast';
+
+export default function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Toaster position="bottom-center" />
+    </div>
+  );
+}
