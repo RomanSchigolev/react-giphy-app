1c4c1ccbc25f6b1a7b17ceb14d0f42da9ced501c
Roman Schigolev <r.shchigolev2018@yandex.ru>
2023-01-14 13:34:40 +0700

    added scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import App from './App';
-import { store } from './store';
+import App from 'App';
+import { store } from 'store';
 
-import './styles.css';
+import 'assets/styles/styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
