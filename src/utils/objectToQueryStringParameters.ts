57ea466087e7501e71386a3f087340bd0e124924
Roman Schigolev <r.shchigolev2018@yandex.ru>
2023-01-20 13:35:39 +0700

    wrapper class for the api

--- src/utils/objectToQueryStringParameters.ts
+++ src/utils/objectToQueryStringParameters.ts
@@ -1,7 +1,7 @@
 import querystring from 'query-string';
 
 export const objectToQueryStringParameters = (object?: Record<string, unknown>): string => {
-  if (typeof object === 'object' && object !== null) {
+  if (typeof object === 'object' && object !== null && !Array.isArray(object)) {
     const resultString = querystring.stringify(object, {
       skipEmptyString: true,
       sort: false,
